8cc7f523fc6f9e5ce56e7e273014543265f620c4
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-01-12 15:47:20 +0530

    Update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['sess_save_path'] = sys_get_temp_dir();
 $config['base_url'] = 'https://akarpublication.herokuapp.com/';
 
 /*
@@ -381,7 +380,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = APPPATH . 'cache/session/';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
