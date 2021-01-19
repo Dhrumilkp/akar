8e84b4854d9696acfcc0e8c33798d9e712871c35
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-01-19 10:28:03 +0530

    Update

--- application/views/Shope/Book_view.php
+++ application/views/Shope/Book_view.php
@@ -12,11 +12,6 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css" integrity="sha512-wR4oNhLBHf7smjy0K4oqzdWumd+r5/+6QO/vDda76MW5iug4PT7v86FoEkySIJft3XA0Ae6axhIvHrqwm793Nw==" crossorigin="anonymous" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css" integrity="sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg==" crossorigin="anonymous" />
 	<meta name="description" content="Aakar Publications - Sculpturing life">
-	<style>
-		.slick-prev:before, .slick-next:before{
-			color:black !important;
-		}
-	</style>
 </head>
 
 <body>
@@ -80,8 +75,8 @@
 			$('.book-img-slider').slick({
 				dots: true,
 				infinite: true,
-				arrows:true,
-				speed: 300,
+				autoplay:true,
+				autoplaySpeed:2000,
 				slidesToShow: 1,
 				adaptiveHeight: true
 			});
@@ -91,7 +86,7 @@
 		$('.category-trigger').click(function(){
 			$('.category-nav').toggleClass('show');
 		});
-		
+
 	</script>
 </body>
 
