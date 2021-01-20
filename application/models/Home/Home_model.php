da7dfbadc1e9d39e202d8a656ccc41e32d734b3e
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-01-20 12:11:10 +0530

    :+1:

--- application/models/Home/Home_model.php
+++ application/models/Home/Home_model.php
@@ -45,14 +45,14 @@ class Home_model extends CI_Model
             $filename=basename($file_name,$ext);
             $newFileName=$filename.time().$ext;
             $final_name = str_replace(' ', '', $newFileName);
-            if (!file_exists('uploads/slider/')) {
-                mkdir('uploads/slider/', 0777, true);
-                move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"uploads/slider/".$final_name);
+            if (!file_exists('uploads/sliders/')) {
+                mkdir('uploads/sliders/', 0777, true);
+                move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"uploads/sliders/".$final_name);
                 array_push($filepath,$final_name);
             }
             else
             {
-                move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"uploads/slider/".$final_name);
+                move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"uploads/sliders/".$final_name);
                 array_push($filepath,$final_name);
             }
             
